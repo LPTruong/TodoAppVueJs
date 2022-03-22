34ab1df05a85fbe081d2ca569e33edb6c03c2a79
SPLPTruong <truonglp@symper.vn>
2022-03-22 09:44:05 +0700

    update

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -4,13 +4,22 @@
   </div>
   <div class = "width-500" id="tasklist">
     <div class ="formTodo">
+      <div class="square" style="--i:0;"> </div>
+      <div class="square" style="--i:1;"> </div>
+      <div class="square" style="--i:2;"> </div>
+      <div class="square" style="--i:3;"> </div>
+      <div class="square" style="--i:4;"> </div>
+      <div class="square" style="--i:5;"> </div>
+      <div class="square" style="--i:6;"> </div>
+
+
       <h3 class="app-header">Danh sách các việc cần làm</h3>
       <div class="app-input">
           <input type="text" autocomplete="off" placeholder="Add new task" class="task-input" v-model="tasks.name" @keyup.enter="newItem">
           <input type="submit" value = " " class="submit-task" title="Add task" @click="newItem">
       </div>
       <ul class="app-list" >
-        <li class="task-list-item" v-for="task in tasks.slice().reverse()" :key="task">
+        <li class="task-list-item" v-for="task of tasks.slice().reverse()" :key="task"> <!-- .slice().reverse()-->
           <input title = "Complete Task" type="checkbox">
           <label class="task-list-item-label">
               <span>{{task.name}}</span>
@@ -24,8 +33,9 @@
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
-    name: 'HelloWorld',
+    name: 'TodoList',
     props: {
       msg: String
     },
@@ -33,9 +43,9 @@
     data() {
       return {
         editedItem: null,
-        
-        title: 'to do list',
+        title: 'To Do List',
         tasks: [],
+        
     
       }
     },
@@ -52,6 +62,18 @@
         this.tasks = JSON.parse(localStorage.getItem("tasks"))
       }
     },
+   
+    created(){
+        axios.get(`http://localhost/nana-framework/list_task`)
+        .then(response => {
+          console.log(response.data)
+          this.tasks = response.data['data']
+         
+        })
+        .catch(e =>{
+          console.log(e)
+        })
+      },
 
     methods: {
       newItem: function() {
@@ -64,11 +86,26 @@
             name: this.tasks.name,
             complete: false
           });
+          axios.post(`http://localhost/nana-framework/insert_task`, { 
+              name: this.tasks.name
+          }).then(response => {
+            console.log(response.data)
+            this.tasks = response.data['data']
+          })
+
+
         }
         else {
           this.tasks[this.editedItem].name = this.tasks.name;
-          this.editedItem = null;
-
+          axios.put(`http://localhost/nana-framework/edit_task`,{
+           
+              id: this.tasks.id, 
+              name: this.tasks.name
+     
+          })
+          console.log(this.tasks.id)
+          console.log(this.tasks.name)
+          this.editedItem = null;      
         }
         
         this.tasks.name = "";
@@ -77,12 +114,26 @@
 
       
       delItem: function(task) {
-        this.tasks.splice(this.tasks.indexOf(task),1)
+        axios.delete(`http://localhost/nana-framework/delete_task`,{
+            data: {
+              id: task.id
+            }
+          }),
+
+        console.log(task.id)
+        this.tasks.splice(this.tasks.indexOf(task),1);
+        
+
       },
+
       editItem: function(task) {
         this.tasks.name = this.tasks[this.tasks.indexOf(task)].name;
-        this.editedItem = this.tasks.indexOf(task)
-      }
+        this.editedItem = this.tasks.indexOf(task);
+        this.tasks.id = task.id;
+        console.log(this.tasks.id)
+      
+      },
+    
 
     }
 
